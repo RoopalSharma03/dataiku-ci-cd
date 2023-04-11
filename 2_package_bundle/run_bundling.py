6c5151d113730b3e5dfd362ea52362a17e6965f9
RoopalSharma03 <128597460+RoopalSharma03@users.noreply.github.com>
2023-04-11 21:23:28 +0530

    changed the code

--- 2_package_bundle/run_bundling.py
+++ 2_package_bundle/run_bundling.py
@@ -21,24 +21,30 @@ auto_apiKey = sys.argv[6]
 
 
 
-
 client = dataikuapi.DSSClient(host,apiKey )
 
 auto_client = dataikuapi.DSSClient(auto_host,auto_apiKey)
 
 test_project = client.get_project(project)
 
-# auto_test_project = auto_client.get_project(project)
+auto_test_deployer = auto_client.get_projectdeployer()
 
 
 
 
 test_project.export_bundle(bundle_id)
 
-# Publish bundle to Project Deployer
+
+
 
 
 # Optional - Export the bundel zip to be archived
 
 test_project.download_exported_bundle_archive_to_file(bundle_id, bundle_id + ".zip")
-auto_client.publish_bundle(bundle_id + ".zip", bundle_id)
+
+
+
+
+# Publish bundle to Project Deployer
+
+auto_test_deployer.upload_bundle(bundle_id + ".zip", bundle_id)
